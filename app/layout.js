e7dab2611f4f869e94ec8aaa21a9132ddbf2e9fb
paramxvaionex <param07singh@gmail.com>
2024-03-07 10:01:58 +0530

    grid and dot bg added

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-950`}>
+      <body className={`${inter.className} bg-gray-950 dot`}>
         {children}
         </body>
     </html>
